8f7d798ec62711cf7e73942579009db959b71a03
Thomas Schreiber <thomas.schreiber@gmx.at>
2020-10-27 13:37:30 +0100

    #1381 apply firefox 82 rmbtws fix

--- src/js/functions.js
+++ src/js/functions.js
@@ -143,11 +143,11 @@ function requestBrowserData(callback, options) {
                             //due to a previous bug in the WebKit library
                             // -> inform the user
                             //https://bugs.webkit.org/show_bug.cgi?id=170463
-                            if (browser_agent.match(/Firefox\/82\.0.*/)) {
-                                $("#popuperror").append(Lang.getString("FirefoxBroken"));
-                                show_errorPopup();
-                                return;
-                            }
+                            //if (browser_agent.match(/Firefox\/82\.0.*/)) {
+                            //    $("#popuperror").append(Lang.getString("FirefoxBroken"));
+                            //    show_errorPopup();
+                            //    return;
+                            //}
 
                             //Firefox 82 won't let user's execute tests
                             //due to a previous bug in the WebKit library
