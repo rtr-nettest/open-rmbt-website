dcc8ecfc9f57578d3266d624831b52daca76aa65
Polina Gurina <p.gurina@icloud.com>
2025-01-14 10:27:32 +0100

    Add stage environment

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  baseUrl: ".",
+  deployedUrl: "https://www.netztest.at",
+  api: {
+    baseUrl: "https://c01.netztest.at",
+    ipv4: "https://c01v4.netztest.at",
+    ipv6: "https://c01v6.netztest.at",
+    cloud: "https://m-cloud.netztest.at",
+  },
+  projectTitle: "RTR-Nettest",
+  certifiedTests: {
+    count: 5,
+    interval: 1,
+  },
+}
