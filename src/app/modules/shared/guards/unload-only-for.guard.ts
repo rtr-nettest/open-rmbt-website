21cd6be5b5fe95fd6e55756c29be8cf3e12d9abc
Polina Gurina <p.gurina@icloud.com>
2025-03-27 19:54:10 +0100

    Switch to Material dialog

--- src/app/modules/shared/guards/unload-only-for.guard.ts
+++ src/app/modules/shared/guards/unload-only-for.guard.ts
@@ -1,22 +1,41 @@
 import { CanDeactivateFn } from "@angular/router"
 import { ERoutes } from "../constants/routes.enum"
-import { MainStore } from "../store/main.store"
 import { inject } from "@angular/core"
+import { MatDialog } from "@angular/material/dialog"
+import { ConfirmDialogComponent } from "../components/confirm-dialog/confirm-dialog.component"
+import { ScrollStrategyOptions } from "@angular/cdk/overlay"
+import { map } from "rxjs"
 
 export const unloadOnlyFor: (
   allowedPaths: ERoutes[]
 ) => CanDeactivateFn<unknown> =
   (allowedPaths) => (component, currentRoute, currentState, nextState) => {
-    const mainStore = inject(MainStore)
+    const dialog = inject(MatDialog)
+    const scrollStrategyOptions = inject(ScrollStrategyOptions)
     if (
       nextState.url &&
       !allowedPaths.some((path) => nextState.url.includes(path))
     ) {
-      if (mainStore.routingEvent() === "popstate") {
-        history.replaceState({ obsolete: true }, "", currentState.url)
-      }
-      globalThis.open(nextState.url, "_self")
-      return false
+      return dialog
+        .open(ConfirmDialogComponent, {
+          data: {
+            text: "Are you sure you want to leave this page? The data may be lost as a result.",
+            canCancel: true,
+          },
+          scrollStrategy: scrollStrategyOptions.noop(),
+        })
+        .afterClosed()
+        .pipe(
+          map((result) => {
+            const proceed = result?.confirmAction === true
+            if (proceed) {
+              setTimeout(() => {
+                location.reload()
+              }, 30)
+            }
+            return proceed
+          })
+        )
     }
     return true
   }
