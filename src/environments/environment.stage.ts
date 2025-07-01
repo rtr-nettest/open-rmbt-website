b6b107eea8ed85c20922d3ba41ad9c4346cf7f0e
Dubravko Jagar <dubravko.jagar@rtr.at>
2025-07-01 16:43:36 +0200

    Test loop mode parameters

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -5,12 +5,12 @@ export const environment = {
     baseUrl: "https://dev2.netztest.at",
   },
   loopModeDefaults: {
-    default_delay: 1,
-    default_tests: 5,
-    max_delay: 1440,
-    max_tests: 500,
-    min_delay: 1,
-    min_tests: 1,
+    default_delay: 15,
+    default_tests: 8,
+    max_delay: 1000,
+    max_tests: 555,
+    min_delay: 5,
+    min_tests: 9,
     exclude_from_result: ["networkType"],
   },
   certifiedDefaults: {
