e027dbb74b76066a5e054a0d59012fd6e2b8efd5
Dubravko Jagar <dubravko.jagar@rtr.at>
2025-07-02 18:12:46 +0200

    Test with production parameter defaults

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -7,17 +7,17 @@ export const environment = {
   loopModeDefaults: {
     default_delay: 15,
     default_tests: 8,
-    max_delay: 1000,
-    max_tests: 555,
+    max_delay: 1440,
+    max_tests: 500,
     min_delay: 5,
-    min_tests: 9,
+    min_tests: 1,
     exclude_from_result: ["networkType"],
   },
   certifiedDefaults: {
-    default_delay: 1,
-    default_tests: 5,
+    default_delay: 15,
+    default_tests: 8,
     max_speed_firefox_mbps: 400,
     exclude_from_result: ["networkType"],
-    disable_graphics: false,
+    disable_graphics: true,
   },
 }
