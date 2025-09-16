02a984f57437fe7b678577fa721aa9c614e7fa5c
Polina Gurina <76707268+polylina@users.noreply.github.com>
2025-09-16 17:37:24 +0200

    Add close button to the bottom of the panel

--- src/app/modules/shared/components/expansion-panel/expansion-panel.component.ts
+++ src/app/modules/shared/components/expansion-panel/expansion-panel.component.ts
@@ -1,14 +1,21 @@
-import { Component, input, model, output } from "@angular/core"
-import { MatExpansionModule } from "@angular/material/expansion"
+import { Component, input, model, output, ViewChild } from "@angular/core"
+import {
+  MatExpansionModule,
+  MatExpansionPanel,
+} from "@angular/material/expansion"
 import { TranslatePipe } from "../../../i18n/pipes/translate.pipe"
+import { MatButtonModule } from "@angular/material/button"
+import { MatIconModule } from "@angular/material/icon"
 
 @Component({
   selector: "app-expansion-panel",
-  imports: [MatExpansionModule, TranslatePipe],
+  imports: [MatExpansionModule, MatButtonModule, MatIconModule, TranslatePipe],
   templateUrl: "./expansion-panel.component.html",
   styleUrl: "./expansion-panel.component.scss",
 })
 export class ExpansionPanelComponent {
+  @ViewChild(MatExpansionPanel) panel!: MatExpansionPanel
+
   title = input.required<string>()
   a11yTitle = input.required<string>()
   expand = output<boolean>()
