55952a73975c7abcf4aa04f9ddfc68f3b9280d85
Polina Gurina <76707268+polylina@users.noreply.github.com>
2026-02-11 17:05:30 +0100

    Replace map overlay with snackbar

--- src/app/modules/map/screens/live-screen/live-screen.component.ts
+++ src/app/modules/map/screens/live-screen/live-screen.component.ts
@@ -1,4 +1,3 @@
-import { AsyncPipe } from "@angular/common"
 import {
   AfterViewInit,
   Component,
@@ -7,20 +6,16 @@ import {
   signal,
 } from "@angular/core"
 import { HeaderComponent } from "../../../shared/components/header/header.component"
-import { HeatmapLegendComponent } from "../../components/heatmap-legend/heatmap-legend.component"
 import { TopNavComponent } from "../../../shared/components/top-nav/top-nav.component"
 import { BreadcrumbsComponent } from "../../../shared/components/breadcrumbs/breadcrumbs.component"
 import { FooterComponent } from "../../../shared/components/footer/footer.component"
 import { MatDialogModule } from "@angular/material/dialog"
-import { SearchComponent } from "../../components/search/search.component"
-import { TranslatePipe } from "../../../i18n/pipes/translate.pipe"
-import { MainContentComponent } from "../../../shared/components/main-content/main-content.component"
 import { SeoComponent } from "../../../shared/components/seo/seo.component"
 import { MapComponent } from "../../components/map/map.component"
 import { LiveService } from "../../../opendata/services/live.service"
 import { FullScreenService } from "../../services/full-screen.service"
 import { FullscreenControl, NavigationControl } from "maplibre-gl"
-import { set } from "ol/transform"
+import { MessageService } from "../../../shared/services/message.service"
 
 @Component({
   selector: "app-live-screen",
@@ -31,7 +26,6 @@ import { set } from "ol/transform"
     FooterComponent,
     MapComponent,
     MatDialogModule,
-    TranslatePipe,
   ],
   templateUrl: "./live-screen.component.html",
   styleUrl: "./live-screen.component.scss",
@@ -39,26 +33,28 @@ import { set } from "ol/transform"
 export class LiveScreenComponent extends SeoComponent implements AfterViewInit {
   private readonly fullScreenService = inject(FullScreenService)
   private readonly liveService = inject(LiveService)
+  private readonly messageService = inject(MessageService)
 
   mapContainerId = "liveMapContainer"
   mapControls = [new NavigationControl(), new FullscreenControl()]
-  showOverlay = signal(true)
   recentMeasurements = this.liveService.recentMeasurements
+  showOverlay = signal(false)
 
   ngAfterViewInit(): void {
     this.liveService.watchMeasurements()
+    this.messageService.openSnackbar("LiveMapOverlayMessage")
+    setTimeout(() => {
+      this.showOverlay.set(true)
+    }, 1500)
+    setTimeout(() => {
+      this.showOverlay.set(false)
+    }, 4500)
   }
 
   @HostListener("document:keypress", ["$event"])
   handleKeyDown(event: KeyboardEvent) {
-    this.showOverlay.set(false)
     if (event.key === "m") {
       this.fullScreenService.toggleFullScreen()
     }
-  }
-
-  @HostListener("document:click")
-  handleClick() {
-    this.showOverlay.set(false)
   }
 }
