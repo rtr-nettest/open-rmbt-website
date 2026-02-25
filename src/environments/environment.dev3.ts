97be8bbfb4f14d043342ff28d79ee6669e95e1af
Dietmar Zlabinger <dzlabing@users.noreply.github.com>
2026-02-25 15:34:01 +0100

    Create environment.dev3.ts

---
+++ src/environments/environment.dev3.ts
@@ -0,0 +1,33 @@
+import { IEnvironment } from "./environment.interface"
+
+export const environment: IEnvironment = {
+  baseUrl: "https://dev3.netztest.at",
+  deployedUrl: "https://dev3.netztest.at",
+  api: {
+    baseUrl: "https://dev3.netztest.at",
+  },
+  loopModeDefaults: {
+    default_delay: 1,
+    default_tests: 5,
+    max_delay: 1440,
+    max_tests: 500,
+    min_delay: 1,
+    min_tests: 1,
+    exclude_from_result: ["networkType"],
+  },
+  certifiedDefaults: {
+    default_delay: 1,
+    default_tests: 5,
+    max_speed_firefox_mbps: 900,
+    exclude_from_result: ["networkType"],
+    disable_graphics: true,
+  },
+  features: {
+    show_server_selection: true,
+    history_filter: true,
+  },
+  matomo: {
+    trackerUrl: "https://piwik.netztest.at",
+    siteId: "42",
+  },
+}
