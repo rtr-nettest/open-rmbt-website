ac2caad33b9d443957fdcb7b6c5d31c840a706cb
Dietmar Zlabinger <dietmar@zlabinger.at>
2026-09-15 09:44:30 +0200

    Process additional response information from the /ip request, add "via proxy" in case of proxy use (e.g. Apple iCloud Relay)
    - "public" IP is greyed out (it is not the client public IP but the IP of the proxy
    - "via proxy" indicates proxy use

--- src/app/modules/home/components/ip-info/ip-info.component.ts
+++ src/app/modules/home/components/ip-info/ip-info.component.ts
@@ -13,6 +13,8 @@ export class IpInfoComponent implements OnDestroy {
   private readonly ipService = inject(IpService)
   ipV4 = this.ipService.ipV4
   ipV6 = this.ipService.ipV6
+  ipV4IsProxy = this.ipService.ipV4IsProxy
+  ipV6IsProxy = this.ipService.ipV6IsProxy
   // TODO: probably will be deleted in the future.
   ipV4Loading = signal<boolean>(false)
   ipV6Loading = signal<boolean>(false)
--- src/app/modules/shared/interfaces/ip-response.interface.ts
+++ src/app/modules/shared/interfaces/ip-response.interface.ts
@@ -1,4 +1,11 @@
 export interface IpResponse {
   v: string
   ip: string
+  nat_type?: string
+  // When true, the client reaches us through a proxy; the proxy_* fields
+  // describe where the proxy is located (they may be empty strings).
+  is_proxy?: boolean
+  proxy_country?: string
+  proxy_region?: string
+  proxy_city?: string
 }
